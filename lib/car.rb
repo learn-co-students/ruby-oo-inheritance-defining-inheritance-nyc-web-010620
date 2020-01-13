8056617a4e509c95765b5137f9a18e67d40c0382
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-01-13 09:51:52 -0500

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,28 @@
 require_relative "./vehicle.rb"
+
+class Car 
+
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, number)
+        @wheel_size = wheel_size
+        @number = number
+    end
+
+    def wheel_size
+        @wheel_size = "small"
+    end
+
+    def wheel_number
+        wheel_number = 2 
+    end
+
+    def go 
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end 
+
+    def fill_up_tank
+        "filling up!"
+    end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,26 @@
 class Vehicle
 
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, number)
+        @wheel_size = wheel_size
+        @number = number
+    end
+
+    def wheel_size
+        @wheel_size = "small"
+    end
+
+    def wheel_number
+        wheel_number = 2 
+    end
+
+    def go 
+        "vrrrrrrrooom!"
+    end 
+
+    def fill_up_tank
+        "filling up!"
+    end
+
 end
